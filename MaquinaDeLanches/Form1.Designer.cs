b146aaa2e896e3a5eb5646ca21aa26710094cc76
MiltomSantos <122657746+MiltomSantos@users.noreply.github.com>
2023-07-14 15:32:25 -0300

    Add files via upload

---
+++ MaquinaDeLanches/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿
+namespace MaquinaDeLanches
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnComprar = new System.Windows.Forms.Button();
+            this.btnBolinho = new System.Windows.Forms.Button();
+            this.btnAgua = new System.Windows.Forms.Button();
+            this.btnRefrigerante = new System.Windows.Forms.Button();
+            this.btnSuco = new System.Windows.Forms.Button();
+            this.btnSalgadinho = new System.Windows.Forms.Button();
+            this.btnEnergetico = new System.Windows.Forms.Button();
+            this.btnDepositar = new System.Windows.Forms.Button();
+            this.txtDepositar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.lblValorMonetario = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.Location = new System.Drawing.Point(157, 271);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(137, 53);
+            this.btnComprar.TabIndex = 0;
+            this.btnComprar.Text = "Comprar";
+            this.btnComprar.UseVisualStyleBackColor = true;
+            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
+            // 
+            // btnBolinho
+            // 
+            this.btnBolinho.Location = new System.Drawing.Point(14, 19);
+            this.btnBolinho.Name = "btnBolinho";
+            this.btnBolinho.Size = new System.Drawing.Size(137, 53);
+            this.btnBolinho.TabIndex = 1;
+            this.btnBolinho.Text = "Bolinho";
+            this.btnBolinho.UseVisualStyleBackColor = true;
+            this.btnBolinho.Click += new System.EventHandler(this.btnBolinho_Click);
+            // 
+            // btnAgua
+            // 
+            this.btnAgua.Location = new System.Drawing.Point(157, 19);
+            this.btnAgua.Name = "btnAgua";
+            this.btnAgua.Size = new System.Drawing.Size(137, 53);
+            this.btnAgua.TabIndex = 2;
+            this.btnAgua.Text = "Agua";
+            this.btnAgua.UseVisualStyleBackColor = true;
+            this.btnAgua.Click += new System.EventHandler(this.btnAgua_Click);
+            // 
+            // btnRefrigerante
+            // 
+            this.btnRefrigerante.Location = new System.Drawing.Point(300, 19);
+            this.btnRefrigerante.Name = "btnRefrigerante";
+            this.btnRefrigerante.Size = new System.Drawing.Size(137, 53);
+            this.btnRefrigerante.TabIndex = 3;
+            this.btnRefrigerante.Text = "Refrigerante";
+            this.btnRefrigerante.UseVisualStyleBackColor = true;
+            this.btnRefrigerante.Click += new System.EventHandler(this.btnRefrigerante_Click);
+            // 
+            // btnSuco
+            // 
+            this.btnSuco.Location = new System.Drawing.Point(14, 103);
+            this.btnSuco.Name = "btnSuco";
+            this.btnSuco.Size = new System.Drawing.Size(137, 53);
+            this.btnSuco.TabIndex = 4;
+            this.btnSuco.Text = "Suco";
+            this.btnSuco.UseVisualStyleBackColor = true;
+            this.btnSuco.Click += new System.EventHandler(this.btnSuco_Click);
+            // 
+            // btnSalgadinho
+            // 
+            this.btnSalgadinho.Location = new System.Drawing.Point(157, 103);
+            this.btnSalgadinho.Name = "btnSalgadinho";
+            this.btnSalgadinho.Size = new System.Drawing.Size(137, 53);
+            this.btnSalgadinho.TabIndex = 5;
+            this.btnSalgadinho.Text = "Salgadinho";
+            this.btnSalgadinho.UseVisualStyleBackColor = true;
+            this.btnSalgadinho.Click += new System.EventHandler(this.btnSalgadinho_Click);
+            // 
+            // btnEnergetico
+            // 
+            this.btnEnergetico.Location = new System.Drawing.Point(300, 103);
+            this.btnEnergetico.Name = "btnEnergetico";
+            this.btnEnergetico.Size = new System.Drawing.Size(137, 53);
+            this.btnEnergetico.TabIndex = 6;
+            this.btnEnergetico.Text = "Energetico";
+            this.btnEnergetico.UseVisualStyleBackColor = true;
+            this.btnEnergetico.Click += new System.EventHandler(this.btnEnergetico_Click);
+            // 
+            // btnDepositar
+            // 
+            this.btnDepositar.Location = new System.Drawing.Point(149, 178);
+            this.btnDepositar.Name = "btnDepositar";
+            this.btnDepositar.Size = new System.Drawing.Size(128, 57);
+            this.btnDepositar.TabIndex = 7;
+            this.btnDepositar.Text = "Depositar";
+            this.btnDepositar.UseVisualStyleBackColor = true;
+            this.btnDepositar.Click += new System.EventHandler(this.btnDepositar_Click);
+            // 
+            // txtDepositar
+            // 
+            this.txtDepositar.Location = new System.Drawing.Point(21, 38);
+            this.txtDepositar.Name = "txtDepositar";
+            this.txtDepositar.Size = new System.Drawing.Size(302, 20);
+            this.txtDepositar.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "3,50";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(206, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "1,50";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(353, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(353, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(206, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(61, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(17, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(180, 22);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Deposite um Valor:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.lblValorMonetario);
+            this.panel1.Controls.Add(this.lblSaldo);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.btnDepositar);
+            this.panel1.Controls.Add(this.txtDepositar);
+            this.panel1.Location = new System.Drawing.Point(578, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(411, 253);
+            this.panel1.TabIndex = 16;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.Location = new System.Drawing.Point(21, 84);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(41, 22);
+            this.lblSaldo.TabIndex = 16;
+            this.lblSaldo.Text = "R$ ";
+            this.lblSaldo.Click += new System.EventHandler(this.lblSaldo_Click);
+            // 
+            // lblValorMonetario
+            // 
+            this.lblValorMonetario.AutoSize = true;
+            this.lblValorMonetario.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorMonetario.Location = new System.Drawing.Point(55, 84);
+            this.lblValorMonetario.Name = "lblValorMonetario";
+            this.lblValorMonetario.Size = new System.Drawing.Size(21, 22);
+            this.lblValorMonetario.TabIndex = 17;
+            this.lblValorMonetario.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.btnBolinho);
+            this.groupBox1.Controls.Add(this.btnComprar);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.btnAgua);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnRefrigerante);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnSuco);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnSalgadinho);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnEnergetico);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(444, 332);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Produtos";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(441, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(213, 25);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Maquina de Lanches";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-5, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1135, 644);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 637);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnComprar;
+        private System.Windows.Forms.Button btnBolinho;
+        private System.Windows.Forms.Button btnAgua;
+        private System.Windows.Forms.Button btnRefrigerante;
+        private System.Windows.Forms.Button btnSuco;
+        private System.Windows.Forms.Button btnSalgadinho;
+        private System.Windows.Forms.Button btnEnergetico;
+        private System.Windows.Forms.Button btnDepositar;
+        private System.Windows.Forms.TextBox txtDepositar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.Label lblValorMonetario;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MaquinaDeLanches/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaquinaDeLanches
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDepositar_Click(object sender, EventArgs e)
+        {
+            double dinheiro = double.Parse(txtDepositar.Text);
+
+            if (dinheiro < 0)
+            {
+                return;
+            }
+
+            double saldo = double.Parse(lblValorMonetario.Text);
+            double soma = dinheiro + saldo;
+            lblValorMonetario.Text = $"{soma}";
+
+        }
+
+        private void lblSaldo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBolinho_Click(object sender, EventArgs e)
+        {
+            btnAgua.BackColor = Color.Gainsboro;
+            btnAgua.ForeColor = Color.Black;
+
+            btnRefrigerante.BackColor = Color.Gainsboro;
+            btnRefrigerante.ForeColor = Color.Black;
+
+            btnSuco.BackColor = Color.Gainsboro;
+            btnSuco.ForeColor = Color.Black;
+
+            btnSalgadinho.BackColor = Color.Gainsboro;
+            btnSalgadinho.ForeColor = Color.Black;
+
+            btnEnergetico.BackColor = Color.Gainsboro;
+            btnEnergetico.ForeColor = Color.Black;
+
+            btnBolinho.BackColor = Color.LightBlue;
+            btnBolinho.ForeColor = Color.DodgerBlue;
+        }
+
+        private void btnAgua_Click(object sender, EventArgs e)
+        {
+            btnBolinho.BackColor = Color.Gainsboro;
+            btnBolinho.ForeColor = Color.Black;
+
+            btnRefrigerante.BackColor = Color.Gainsboro;
+            btnRefrigerante.ForeColor = Color.Black;
+
+            btnSuco.BackColor = Color.Gainsboro;
+            btnSuco.ForeColor = Color.Black;
+
+            btnSalgadinho.BackColor = Color.Gainsboro;
+            btnSalgadinho.ForeColor = Color.Black;
+
+            btnEnergetico.BackColor = Color.Gainsboro;
+            btnEnergetico.ForeColor = Color.Black;
+
+            btnAgua.BackColor = Color.LightBlue;
+            btnAgua.ForeColor = Color.DodgerBlue;
+        }
+
+        private void btnRefrigerante_Click(object sender, EventArgs e)
+        {
+            btnAgua.BackColor = Color.Gainsboro;
+            btnAgua.ForeColor = Color.Black;
+
+            btnBolinho.BackColor = Color.Gainsboro;
+            btnBolinho.ForeColor = Color.Black;
+
+            btnSuco.BackColor = Color.Gainsboro;
+            btnSuco.ForeColor = Color.Black;
+
+            btnSalgadinho.BackColor = Color.Gainsboro;
+            btnSalgadinho.ForeColor = Color.Black;
+
+            btnEnergetico.BackColor = Color.Gainsboro;
+            btnEnergetico.ForeColor = Color.Black;
+
+            btnRefrigerante.BackColor = Color.LightBlue;
+            btnRefrigerante.ForeColor = Color.DodgerBlue;
+        }
+
+        private void btnSuco_Click(object sender, EventArgs e)
+        {
+            btnAgua.BackColor = Color.Gainsboro;
+            btnAgua.ForeColor = Color.Black;
+
+            btnRefrigerante.BackColor = Color.Gainsboro;
+            btnRefrigerante.ForeColor = Color.Black;
+
+            btnBolinho.BackColor = Color.Gainsboro;
+            btnBolinho.ForeColor = Color.Black;
+
+            btnSalgadinho.BackColor = Color.Gainsboro;
+            btnSalgadinho.ForeColor = Color.Black;
+
+            btnEnergetico.BackColor = Color.Gainsboro;
+            btnEnergetico.ForeColor = Color.Black;
+
+            btnSuco.BackColor = Color.LightBlue;
+            btnSuco.ForeColor = Color.DodgerBlue;
+        }
+
+        private void btnSalgadinho_Click(object sender, EventArgs e)
+        {
+            btnAgua.BackColor = Color.Gainsboro;
+            btnAgua.ForeColor = Color.Black;
+
+            btnRefrigerante.BackColor = Color.Gainsboro;
+            btnRefrigerante.ForeColor = Color.Black;
+
+            btnSuco.BackColor = Color.Gainsboro;
+            btnSuco.ForeColor = Color.Black;
+
+            btnBolinho.BackColor = Color.Gainsboro;
+            btnBolinho.ForeColor = Color.Black;
+
+            btnEnergetico.BackColor = Color.Gainsboro;
+            btnEnergetico.ForeColor = Color.Black;
+
+            btnSalgadinho.BackColor = Color.LightBlue;
+            btnSalgadinho.ForeColor = Color.DodgerBlue;
+        }
+
+        private void btnEnergetico_Click(object sender, EventArgs e)
+        {
+            btnAgua.BackColor = Color.Gainsboro;
+            btnAgua.ForeColor = Color.Black;
+
+            btnRefrigerante.BackColor = Color.Gainsboro;
+            btnRefrigerante.ForeColor = Color.Black;
+
+            btnSuco.BackColor = Color.Gainsboro;
+            btnSuco.ForeColor = Color.Black;
+
+            btnSalgadinho.BackColor = Color.Gainsboro;
+            btnSalgadinho.ForeColor = Color.Black;
+
+            btnBolinho.BackColor = Color.Gainsboro;
+            btnBolinho.ForeColor = Color.Black;
+
+            btnEnergetico.BackColor = Color.LightBlue;
+            btnEnergetico.ForeColor = Color.DodgerBlue;
+        }
+
+        private void btnComprar_Click(object sender, EventArgs e)
+        {
+            //bolinho
+            if(btnBolinho.BackColor == Color.LightBlue)
+            {
+                double dinheiro = double.Parse(lblValorMonetario.Text);
+
+                if (dinheiro == 0 || dinheiro <= 3.4)
+                {
+                    MessageBox.Show("Sem saldo suficiente");
+                    return;
+                }
+
+                double bolinho = 3.5;
+                double conta = dinheiro - bolinho;
+                MessageBox.Show("Você comprou o bolinho!");
+                lblValorMonetario.Text = $"{conta}";
+
+            }
+
+            //agua
+            if (btnAgua.BackColor == Color.LightBlue)
+            {
+                double dinheiro = double.Parse(lblValorMonetario.Text);
+
+                if (dinheiro <= 1.4)
+                {
+                    MessageBox.Show("Sem saldo suficiente");
+                    return;
+                }
+
+                double agua = 1.5;
+                double conta = dinheiro - agua;
+                MessageBox.Show("Você comprou a agua!");
+                lblValorMonetario.Text = $"{conta}";
+
+            }
+
+            //Refri
+            if (btnRefrigerante.BackColor == Color.LightBlue)
+            {
+                double dinheiro = double.Parse(lblValorMonetario.Text);
+
+                if (dinheiro < 3)
+                {
+                    MessageBox.Show("Sem saldo suficiente");
+                    return;
+                }
+
+                double bolinho = 3;
+                double conta = dinheiro - bolinho;
+                MessageBox.Show("Você comprou o refrigerante!");
+                lblValorMonetario.Text = $"{conta}";
+
+            }
+
+            //suco
+            if (btnSuco.BackColor == Color.LightBlue)
+            {
+                double dinheiro = double.Parse(lblValorMonetario.Text);
+
+                if (dinheiro < 2)
+                {
+                    MessageBox.Show("Sem saldo suficiente");
+                    return;
+                }
+
+                double bolinho = 2;
+                double conta = dinheiro - bolinho;
+                MessageBox.Show("Você comprou o Suco!");
+                lblValorMonetario.Text = $"{conta}";
+
+            }
+
+            //salgadinho
+            if (btnSalgadinho.BackColor == Color.LightBlue)
+            {
+                double dinheiro = double.Parse(lblValorMonetario.Text);
+
+                if (dinheiro < 4)
+                {
+                    MessageBox.Show("Sem saldo suficiente");
+                    return;
+                }
+
+                double bolinho = 4;
+                double conta = dinheiro - bolinho;
+                MessageBox.Show("Você comprou o Salgadinho!");
+                lblValorMonetario.Text = $"{conta}";
+
+            }
+
+            //energetico
+            if (btnEnergetico.BackColor == Color.LightBlue)
+            {
+                double dinheiro = double.Parse(lblValorMonetario.Text);
+
+                if (dinheiro < 3)
+                {
+                    MessageBox.Show("Sem saldo suficiente");
+                    return;
+                }
+
+                double bolinho = 3;
+                double conta = dinheiro - bolinho;
+                MessageBox.Show("Você comprou o energetico!");
+                lblValorMonetario.Text = $"{conta}";
+
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
